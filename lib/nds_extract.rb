03d4e15adb165078d1f5eefb21b7f5aa0c2b707e
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 04:10:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -85,6 +85,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+            [
+              { :name => "Byron Poodle", :movies => [
+                { :title => "At the park" },
+                { :title => "On the couch" },
+                ]
+            ]
+  
   
   output = []
   
