02bf3bfea018f8d188ba2cb6156c3c64a0b07fcd
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 04:06:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -91,7 +91,7 @@ def movies_with_directors_set(source)
   pp source
   
   source.each do |movie|
-    output << movie_with_director_name(source[:name], movie)
+    output << movie_with_director_name(movie[:name], movie[:movies])
   end
   
   output
